36eb926d64012e1827acc6d6bb8a10f0e19c3524
Leonardo Martinez <leonardomartinez@Leonardos-Air.home>
2023-08-01 00:53:08 -0700

    created data.js file, imported puppyList from data.js to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import './App.css'
+import { puppyList } from './data'
+import { useState } from 'react'
 
 function App() {
-
+  const [puppies, setPuppies] = useState(puppyList);
+  console.log('puppyList: ', puppyList)
+  
   return (
-    <div></div>
-  )
+    <div className='App'>
+      {
+        puppies.map((puppy) => {
+          return <p key={puppy.id}>{puppy.name}</p>;
+        })
+      }
+    </div>
+  );
 }
 
 export default App
